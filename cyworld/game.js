b839faceda6e97e9da1f4ba9e6d3d15ad514c7e0
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2024-04-26 16:40:52 +0900

    made wordchain js

---
+++ cyworld/game.js
@@ -0,0 +1,20 @@
+const startWord = () => {
+	let inputWord = document.getElementById("wordchain_input_box").value
+	let currentWord = document.getElementById("wordchain_current_word").innerText
+	
+	let lastWord = currentWord[currentWord.length - 1];
+	let firstWord = inputWord[0];
+
+	if (lastWord === firstWord)
+	{
+		document.getElementById("wordchain_result").innerText = "정답입니다!"
+		document.getElementById("wordchain_current_word").innerText = inputWord;
+		document.getElementById("wordchain_input_box").value = "";
+	}
+	else
+	{
+		document.getElementById("wordchain_result").innerText = "땡!"
+		document.getElementById("wordchain_input_box").value = "";
+		
+	}
+}
